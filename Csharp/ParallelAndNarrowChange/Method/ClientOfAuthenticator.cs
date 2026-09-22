3c5062a82253aa7946935e5124ec387ee8dd5005
David Ferrell <chrstrock@gmail.com>
2026-09-22 11:59:40 -0400

    Convert ClientOfAuthenticator public constructor to primary constructor.

--- Csharp/ParallelAndNarrowChange/Method/ClientOfAuthenticator.cs
+++ Csharp/ParallelAndNarrowChange/Method/ClientOfAuthenticator.cs
@@ -5,15 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ParallelAndNarrowChange.Method {
-public class ClientOfAuthenticator {
-    private AuthenticationService authenticationService;
-
+public class ClientOfAuthenticator(AuthenticationService authenticationService)
+{
     public static void Main(String[] args) {
         new ClientOfAuthenticator(new AuthenticationService()).Run();
-    }
-
-    public ClientOfAuthenticator(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
     }
 
     public void Run() {
